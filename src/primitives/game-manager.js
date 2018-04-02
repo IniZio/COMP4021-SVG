7e2eee17fd2d5a3199a5e8004c6bc37a97bc3f7e
IniZio <newman.kcchow@gmail.com>
2018-04-02 21:40:16 +0800

    feat: game-manager constructor (#5)
    - refactors html for easier templating
    - adds simplest game manager constructor

---
+++ src/primitives/game-manager.js
@@ -0,0 +1,41 @@
+import SVG from 'svg.js'
+
+class GameManager {
+  constructor ({el, empty}) {
+    if (!SVG.supported) {
+      alert('SVG not supported')
+      return
+    }
+
+    this.$el = el
+    this.$empty = empty
+    this.gameObjects = []
+
+    return this
+  }
+
+  // Resets scene to empty
+  reset () {
+    const emptyClone = SVG.adopt(document.getElementById(this.$empty).cloneNode(true))
+    emptyClone.attr('id', SVG.get(this.$el).attr('id'))
+
+    SVG.get(this.$el).remove()
+    SVG.get(this.$context).add(emptyClone)
+  }
+
+  mount (context) {
+    this.$context = context
+
+    this.reset()
+
+    (function gameLoop (){
+      this.update()
+    })()
+  }
+
+  update () {
+    this.gameObjects.map(object => object.update && object.update())
+  }
+}
+
+export default GameManager
