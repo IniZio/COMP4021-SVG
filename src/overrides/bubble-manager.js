76bd69e350bce761b84f0cf2157f61d77572729b
IniZio <newman.kcchow@gmail.com>
2018-04-03 23:12:41 +0800

    feat(props): spawn props on game scene once initialized (#12)

--- src/overrides/bubble-manager.js
+++ src/overrides/bubble-manager.js
@@ -1,7 +1,9 @@
 import SVG from 'svg.js'
 
+import {getRandomInt} from '../utils'
 import GameManager from '../primitives/game-manager'
 import Player from '../overrides/player'
+// import Food from '../objs/food'
 
 class BubbleManager extends GameManager {
   constructor () {
@@ -14,13 +16,25 @@ class BubbleManager extends GameManager {
       },
       game (manager) {
         manager.addGameObject(new Player({x: 0, y: 10, svg: SVG.get('main1').clone()}))
+        manager.scheduleProps()
       },
       gameover (manager) {
-        SVG.get('restart_button').click(() => {
-          manager.reset('game')
-        })
+        SVG.get('restart_button').click(manager.reset)
       }
     }
+  }
+
+  scheduleProps () {
+    const scheduler = setInterval(() => {
+      // TODO: use Food class once implemented as aligned with GameObject
+      this.addGameObject(new Player({
+        x: getRandomInt(10, 990),
+        y: getRandomInt(10, 490),
+        svg: getRandomInt(-1, 2) ? SVG.get('food').clone() : SVG.get('booster').clone(),
+      }))
+    }, 3000)
+    this.$intervals.push(scheduler)
+    return scheduler
   }
 }
 
